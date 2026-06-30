ea89aa94e88be3f542e5b1919bc192643d124e2a
yuu8409-oss <yuu8409@techno.dge.es.kr>
2026-06-30 13:50:32 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const cacheName='udl-growth-v1';const assets=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',event=>{event.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets)))});self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)))})
