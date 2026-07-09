d2246281d168940f46630cfba6a646c32c79ccaa
yuu8409-oss <yuu8409@techno.dge.es.kr>
2026-07-09 13:09:15 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,29 @@
-self.addEventListener('install',event=>self.skipWaiting());
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>new Response('새로고침해 주세요.',{headers:{'Content-Type':'text/plain; charset=utf-8'}})));});
+const CACHE_NAME='udl-growth-v2-100';
+const APP_SHELL=['./','./index.html','./styles.css?v=100','./app.js?v=100','./manifest.webmanifest?v=100','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        if(response.ok&&new URL(event.request.url).origin===self.location.origin){
+          const copy=response.clone();
+          caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+        }
+        return response;
+      })
+      .catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))
+  );
+});
